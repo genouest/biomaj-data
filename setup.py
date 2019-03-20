11036ebfc94f67391a085e09d8dc231765507282
Olivier Sallou <olivier.sallou@irisa.fr>
2019-03-20 10:02:37 +0100

    add flybase bank templates

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ config = {
     'author': 'Olivier Sallou',
     'url': 'https://github.com/genouest/biomaj-data',
     'author_email': 'olivier.sallou@irisa.fr',
-    'version': '3.1.0',
+    'version': '3.1.1',
      'classifiers': [
         # How mature is this project? Common values are
         #   3 - Alpha
